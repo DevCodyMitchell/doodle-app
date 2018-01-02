cc1afba380c98c916a1b3e1b35108086d9469e1e
DevCodyMitchell <35013912+DevCodyMitchell@users.noreply.github.com>
2018-01-02 12:01:13 -0500

    V 1.0
    Added mobile bug-fixes

--- sketch.js
+++ sketch.js
@@ -1,15 +1,36 @@
+//Web Drawing App
+
 function setup() { 
-  createCanvas(windowWidth, windowHeight);
+  //Create fullscreen canvas
+  createCanvas(windowWidth, windowHeight-30);
   background(255);
   strokeWeight(5);
-  text("Doodle - Cody Mitchell \n\nPRESS ANY KEY TO CLEAR",20,20);
+  text("Doodle v1.0 - Cody Mitchell \n\nPRESS ANY KEY TO CLEAR",20,20);
 } 
 
 function mouseDragged() {
+  //Draw line on mouse
 	line(mouseX,mouseY,pmouseX,pmouseY);
 }
 
 function keyPressed() {
   background(255);
-  text("Press a key to Clear",20,20);
+}
+
+//Scroll fixes for mobile
+function touchStarted() {
+	if (!fullscreen() && windowWidth<480) {
+    stroke(255,0,0);
+  	fullscreen(true);
+  }
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+
+document.ontouchmove = function(e) {
+  if (windowWidth<480) { //Check if Mobile
+	e.preventDefault();
+  }
 }
